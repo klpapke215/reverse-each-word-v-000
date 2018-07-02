5d368f2699b116737528fae3710f82aed1a01bc8
Kristen Papke <klpapke215@gmail.com>
2018-07-02 02:05:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,7 @@
-sentence = "Hi again, just making sure it's reversed!"
-
 def reverse_each_word(sentence)
   new_sentence = sentence.split
   new_sentence.collect do |word|
     word.reverse!
-    puts word
   end
-  puts new_sentence.join
+  new_sentence.join
 end
-
-reverse_each_word(sentence)
